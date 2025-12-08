e321e411c9e44e78687d6778aee0f9b88c12759d
Chibuzor <chibuzormekalam@gmail.com>
2025-12-08 11:33:41 +0100

    adding cart swagger

--- routers/cartRouters.js
+++ routers/cartRouters.js
@@ -8,8 +8,34 @@ const {
 const { isUser } = require('../middleware/auth')
 const cartRouter = express.Router()
 
-
-
+/**
+ * @swagger
+ * /addToCart:
+ *   post:
+ *     summary:
+ *     tags:
+ *       - Carts
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                userid:
+ *                 type: string
+ *                productid:
+ *                  type: string
+ *                color:
+ *                  type: string
+ *                size:
+ *                  type: string
+ *                quantity:
+ *                  type: string
+ *     responses:
+ *       200:
+ *         description:
+ */
 cartRouter.post('/addcart', isUser, addToCart)
 
 cartRouter.get('/getcart/:userid', isUser, getCart)
